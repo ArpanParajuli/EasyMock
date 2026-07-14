df613a7a422eeb08665b1a8c452e4f98dcac31b9
ArpanParajuli <arpan.parajuli24@radiantcollege.edu.np>
2026-07-14 13:46:43 +0545

    EasyMock Backend Project Setup

---
+++ src/backend/EasyMock/EasyMock.Api/Controllers/DashBoardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyMock.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashBoardController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("this is dashboard controller");
+        }
+    }
+}
